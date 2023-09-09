69e72c9c76b3831346470646130ccde5dc018dbc
victorvulchev <62486970+victorvulchev@users.noreply.github.com>
2023-09-09 16:17:10 +0300

    Add files via upload

--- lists_task9.py
+++ lists_task9.py
@@ -8,6 +8,16 @@ for i in range(1,rows + 1):
         numb += rows
         print(numb, end=" ")
     print(" ")
+#don't know how to do the other
+# +4 then -1
+print("-" * 10)
 
+for i in range(1, rows + 1):
+    for j in range(1, colums + 1):
+        if j % 2 == 1:
+            print((j - 1) * rows + i, end=" ")
+        else:
+            print(j * rows - i + 1, end=" ")
+    print()
 
 
